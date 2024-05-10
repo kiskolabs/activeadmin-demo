739fd1842eda78ba20f5aac3117b455cec6414e7
Andrei Makarov <andrei@kiskolabs.com>
2024-05-10 10:49:03 +0300

    Update stimulus configuration

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,17 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+
+import { Application } from "@hotwired/stimulus";
+import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading";
+
+const application = Application.start();
+
+application.debug = false;
+window.Stimulus = application;
+
+lazyLoadControllersFrom("controllers", application);
+
+document.addEventListener("turbo:load", (event) => {
+  console.log("Turbo is ready!");
+});
+
+export { application };
